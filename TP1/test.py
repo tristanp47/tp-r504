cb5ad0798ca4429339ac3d5c9c0c00fd2a296b2b
tristanp47 <tristan.peyredieu@univ-rouen.fr>
2023-09-08 15:20:10 +0200

    msg

--- TP1/test.py
+++ TP1/test.py
@@ -7,9 +7,9 @@ def test_1():
 	
 def test_2():
 	assert f.puissance(2,-6) == 0.015625
-	assert f.puissance(-3,2) == 9
+	assert f.puissance(-3,2) == -9
 def test_3():
-	assert f.puissance(2,0) == 1
+	assert f.puissance(-2,0) == -1
 	assert f.puissance(2,-2) == 1/2**2
 	assert f.puissance(-2,3) == -8
 	
