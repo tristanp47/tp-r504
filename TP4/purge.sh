b58d53c3bd396479cf18517ed8affe5c0a9b9955
tristanp47 <tristan.peyredieu@univ-rouen.fr>
2023-10-02 08:44:05 +0200

    msg

--- TP4/purge.sh
+++ TP4/purge.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+n=$(docker ps -q)
+if [$n == 0]
+then
+	echo "Aucun conteneur lancé"
+else 
+	docker stop $(docker ps -q)
+	if [$? != 0]
+	then
+		exit 2
+fi
+
 docker rm -f $(docker ps -aq)
+if [$? != 0]
+then
+	exit 2
+fi
 
 docker network prune -f
-
+if [$? != 0]
+then
+	exit 2
+fi
 docker volume prune -f
+if [$? != 0]
+then
+	exit 2
+fi
 
 docker image prune -af
+if [$? != 0]
+then
+	exit 2
+fi
