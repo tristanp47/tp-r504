c5a55b9b49d9c1d14b07e63f00667dfa30e181ad
tristanp47 <tristan.peyredieu@univ-rouen.fr>
2023-12-08 10:53:29 +0100

    nv

--- TP10/Client1.java
+++ TP10/Client1.java
@@ -22,6 +22,17 @@ public class Client1
 	
 			System.out.println( "Response Line: " + resp.getStatusLine() );
 			System.out.println( "Response Code: " + resp.getStatusLine().getStatusCode() );
+			
+			BufferedReader rd = new BufferedReader( new InputStreamReader( resp.getEntity().getContent()) );
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null)
+			{
+				result.append(line);
+				result.append("\n");
+			}
+			String page = result.toString();
+			System.out.println( page );
 		
 		} 
 		catch ( Exception ex ) {
---
+++ TP10/OMDbJson.sh
@@ -0,0 +1,1 @@
+./run.sh "www.omdbapi.com/?apikey=751ea6aa&t=gladiator" > b
