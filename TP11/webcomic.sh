b9c5b54f3085d6d2089e161f1486d0f4ca48f2c5
tristanp47 <tristan.peyredieu@univ-rouen.fr>
2023-12-18 11:17:38 +0100

    maj

--- TP11/webcomic.sh
+++ TP11/webcomic.sh
@@ -3,10 +3,10 @@ comic_number=$1
 
 comic_url="http://xkcd.com/$comic_number/"
 
-wget_output=$(wget -qO- "$comic_url")
+get_output=$(wget -qO- "$comic_url")
 
-image_line=$(echo "$wget_output" | grep -oP '<meta property="og:image" content="\K[^"]+')
+image=$(echo "$get_output" | grep -oP '<meta property="og:image" content="\K[^"]+')
 
-wget "$image_line" -O xkcd_strip.png
+wget "$image" -O xkcd_strip.png
 
 exo-open xkcd_strip.png
